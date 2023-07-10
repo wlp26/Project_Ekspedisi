90952b6028578b9bb6c5cec879eeed62f9d65abd
wlp26 <c14200150@john.petra.ac.id>
2023-07-10 17:54:15 +0700

    Update laporanKerusakan.dart

--- lib/screens/laporanKerusakan.dart
+++ lib/screens/laporanKerusakan.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-const List<String> list = <String>['One', 'Two', 'Three', 'Four'];
+const List<String> list = <String>[
+  'Pick up',
+  'Truk sedang',
+  'Truk besar',
+  'Four'
+];
 
 class LaporanKerusakan extends StatefulWidget {
   _LaporanKerusakan createState() => _LaporanKerusakan();
@@ -21,30 +27,157 @@ class _LaporanKerusakan extends State<LaporanKerusakan> {
             ),
             title: Text("Laporan Kerusakan"),
             backgroundColor: Colors.black),
-        body: Container(
-            child: Column(children: [
-          DropdownButton<String>(
-            value: dropDownValue,
-            icon: const Icon(Icons.arrow_downward),
-            elevation: 16,
-            style: const TextStyle(color: Colors.deepPurple),
-            underline: Container(
-              height: 2,
-              color: Colors.deepPurpleAccent,
-            ),
-            onChanged: (String? value) {
-              // This is called when the user selects an item.
-              setState(() {
-                dropDownValue = value!;
-              });
-            },
-            items: list.map<DropdownMenuItem<String>>((String value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: Text(value),
-              );
-            }).toList(),
-          )
-        ])));
+        body: Center(
+            child: Container(
+                padding: EdgeInsets.only(top: 8.0),
+                child: Column(children: [
+                  Container(
+                    child: Text(
+                      "Jenis Kendaraan",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    padding: EdgeInsets.only(top: 20.0, bottom: 5.0),
+                  ),
+                  Container(
+                    width: 250, // Mengatur lebar container
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(10),
+                          topRight: Radius.circular(10),
+                          bottomLeft: Radius.circular(10),
+                          bottomRight: Radius.circular(10)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: Offset(0, 3), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                    child: DropdownButtonHideUnderline(
+                      child: DropdownButtonFormField<String>(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          enabledBorder: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                          errorBorder: InputBorder.none,
+                          disabledBorder: InputBorder.none,
+                          contentPadding: EdgeInsets.symmetric(
+                              vertical: 10, horizontal: 16),
+                        ),
+                        value: dropDownValue,
+                        icon: Icon(Icons.arrow_downward),
+                        iconSize: 24,
+                        elevation: 8,
+                        style: TextStyle(color: Colors.black),
+                        onChanged: (String? value) {
+                          setState(() {
+                            dropDownValue = value!;
+                          });
+                        },
+                        items:
+                            list.map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: Text(
+                      "Nomor Plat",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    padding: EdgeInsets.only(top: 20.0, bottom: 5.0),
+                  ),
+                  Container(
+                      width: 250, // Mengatur lebar container
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                            topRight: Radius.circular(10),
+                            bottomLeft: Radius.circular(10),
+                            bottomRight: Radius.circular(10)),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: Offset(0, 3), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          enabledBorder: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                          errorBorder: InputBorder.none,
+                          disabledBorder: InputBorder.none,
+                          contentPadding: EdgeInsets.symmetric(
+                              vertical: 10, horizontal: 16),
+                        ),
+                      )),
+                  Container(
+                    child: Text(
+                      "Deskripsi Kerusakan",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    padding: EdgeInsets.only(top: 20.0, bottom: 5.0),
+                  ),
+                  Container(
+                      width: 250, // Mengatur lebar container
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                            topRight: Radius.circular(10),
+                            bottomLeft: Radius.circular(10),
+                            bottomRight: Radius.circular(10)),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: Offset(0, 3), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          enabledBorder: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                          errorBorder: InputBorder.none,
+                          disabledBorder: InputBorder.none,
+                          contentPadding: EdgeInsets.symmetric(
+                              vertical: 10, horizontal: 16),
+                        ),
+                        minLines: 1,
+                        maxLines: 5,
+                        maxLength: 250,
+                      )),
+                  SizedBox(
+                    height: 50.0,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Add your desired functionality when the button is pressed
+                    },
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(120.0, 50.0),
+                      backgroundColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                    child: Text('Start'),
+                  ),
+                ]))));
   }
 }
