a2c9aaf3c1707d08daceb571b79e6519e7d4ae40
wlp26 <c14200150@john.petra.ac.id>
2023-07-11 12:27:31 +0700

    Create lupaPassword.dart

---
+++ lib/screens/lupaPassword.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LupaPassword extends StatefulWidget {
+  @override
+  _LupaPassword createState() => _LupaPassword();
+}
+
+class _LupaPassword extends State<LupaPassword> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
