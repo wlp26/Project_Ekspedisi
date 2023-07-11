f7a536585cf662fa8a4030c2c73d9ff1a3ac92e1
justinrampengan <105626099+OrangJenius@users.noreply.github.com>
2023-07-11 12:29:46 +0700

    pusing

--- lib/screens/pengiriman.dart
+++ lib/screens/pengiriman.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Pengiriman extends StatefulWidget {
   @override
@@ -6,24 +7,34 @@ class Pengiriman extends StatefulWidget {
 }
 
 class _Pengiriman extends State<Pengiriman> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar:
-            AppBar(title: Text("Pengiriman"), backgroundColor: Colors.black),
-        body: Container(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-              Container(
-                padding: EdgeInsets.only(top: 10),
-                child: Text(
-                  "No Order",
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ])));
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.green[700],
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Maps Sample App'),
+          elevation: 2,
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ),
+      ),
+    );
   }
 }
